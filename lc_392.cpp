60d0a0d97fb5c86ecb9da0e2fd595e44c1160f72
wreckage0907 <girish.raghav2004@gmail.com>
2023-09-22 09:17:24 +0530

    Is subsequence?

---
+++ lc_392.cpp
@@ -0,0 +1,14 @@
+#pragma GCC optimize("Ofast,funroll-loops")
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+};
