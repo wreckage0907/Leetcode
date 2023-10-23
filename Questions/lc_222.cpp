8869fe2655b3615d0fbf1c5e4cda0dd60973a371
wreckage0907 <girish.raghav2004@gmail.com>
2023-10-23 18:55:58 +0530

    Count Complete Tree Nodes

---
+++ Questions/lc_222.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
